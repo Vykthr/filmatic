e8bb4952251988062cad5f6b68d326ff31d93032
vykthr <victorawoke07@gmail.com>
2021-04-12 09:47:20 +0100

    changes

--- src/redux/api/movieApi.js
+++ src/redux/api/movieApi.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-
+const $axios = axios.create({
+  headers: {
+    "Access-Control-Allow-Origin" : "*",
+  },
+})
 export default {
   fetchMovies() {
-    return axios.get(`https://filmatic-api.netlify.app/`);
+    return $axios.get(`https://filmatic-api.netlify.app/`);
   },
   rentMovie(payload) {
-    return axios.post(`http://localhost:4000/movie/rent`, (payload));
+    return $axios.post(`https://filmatic-api.netlify.app/movie/rent`, (payload));
   },
 };
